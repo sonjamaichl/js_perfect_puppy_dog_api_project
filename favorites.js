20f29336fee4f3354f5df73e96dc490347f03010
sonjamaichl <sonja.maichl@gmail.com>
2023-04-25 10:23:51 +0200

    bug fixed: favorites list is displayed again when opening the page

--- favorites.js
+++ favorites.js
@@ -1,5 +1,6 @@
 let myFavoriteDogs = (typeof(localStorage.getItem('favoriteDogs'))=='undefined')? [] : JSON.parse(localStorage.getItem('favoriteDogs'));
 
+console.log(myFavoriteDogs);
 //document.getElementById('favoritesList').innerText = (myFavoriteDogs.length === 0)? 'You have no favorites saved yet.' : ''; // OR: `You have ${myFavoriteDogs.length} favorites:`';
 
 if (myFavoriteDogs.length > 0) {
